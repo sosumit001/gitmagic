da8989bd85177cda52d775af4ca0e80740a2c469
Hello Isf <23234234@gmail.com>
2022-04-11 02:28:22 +0530

    added to new branch

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
-//load content
-
-import Header from "./component/header.js";
-import Home from "./component/home.js";
-import Brandlogo from "./component/brand-logo.js";
 window.onload = () =>{
-    let navWrapper = document.querySelector('.nav-wrapper');
-    let mobileDemo = document.querySelector('#mobile-demo');
-    let homeSlogen = document.querySelector('#home-page-intro')
-    let brandSection = document.querySelector("#brand-section");
-    
-
-    //add content to header 
-    navWrapper.innerHTML = Header.header;
-    mobileDemo.innerHTML = Header.mobiledemo;
-
-    //onscroll event 
-    window.addEventListener('scroll',()=>{
-        if(window.pageYOffset > 31){
-            navWrapper.style = "display:none;"
-        }else
+    let headerIcon = document.querySelector('#header-icon');
+    let sideNav = document.querySelector('#sidenav')
+    let topNav = document.querySelector('.header ul')
+    let homeSlogen = document.querySelector('#home-slogen h1');
+    let header = document.querySelector('.header');
+    let topNavEle = document.querySelectorAll('.header ul a');
+    let current = true;
+    let click = true;
+    //query on resize 
+    console.log(topNavEle)
+    window.addEventListener('resize',()=>{
+       if(click)
+       {
+        if(current && window.innerWidth> 1000)
         {
-            navWrapper.style = "display:block";
+            headerIcon.style = "display:none;"
+        topNav.style = "display:flex;"
+        current = false;
         }
+        if(!current && window.innerWidth <= 1000)
+        {
+            headerIcon.style = "display:block;"
+            topNav.style = "display:none;"
+            current = true;
+        }
+      
+       }
     })
 
-    //home page intro
-    homeSlogen.innerHTML = Home.homeIntro;
-
-    //add brand logo
-    brandSection.innerHTML = Brandlogo.brandLogo;
+    headerIcon.addEventListener('click',()=>{
+        if(current.innerHeight < 789)
+        {
+            topNav.style = "width:60%"
+        }
+        {
+            click = false;
+            setTimeout(() => {
+                sideNav.style.cssText = "display:flex; opacity:1; width:30%;"
+            current = false;
+            }, 20);
+        }
+    })
+    window.addEventListener('click',()=>{
+        if(!current)
+        {
+            click = true;
+            sideNav.style.cssText = "display:none; opacity:0; width:0;"
+            current = true;
+        }
+    })
+    //header on scroll
+    window.addEventListener('scroll',()=>{
+        if(window.pageYOffset>14)
+        {
+            header.style = "background-color:orange;"
+            topNavEle.style = "color:yellow";
+        }
+        else{
+            header.style.backgroundColor = "rgba(0,0,0,0)";
+        }
+    })
 }
