80a62f6ff09be639993e0a0da2d8004d0db23b5c
Hello Isf <23234234@gmail.com>
2022-04-10 19:21:32 +0530

    compnent add

---
+++ component/brand-logo.js
@@ -0,0 +1,9 @@
+const Brandlogo = {
+    brandLogo : `   <div id="brand-img">
+    <img src="./content/image/brand_img_1.svg" alt="">
+<img src="./content/image/brand_img_1.svg" alt="">
+<img src="./content/image/brand.svg" alt="">
+<img src="./content/image/brand_img_4.svg" alt="">
+</div>`
+};
+export default Brandlogo;
---
+++ component/header.js
@@ -0,0 +1,21 @@
+const Header = {
+    header: `  <nav>
+    <div class="hello nav-wrapper black">
+      <a herf ="#!"  class = "brand-logo" ><img style = "margin-top:8px; cursor:pointer;" src = "./content/image/logo.svg" /></a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fa-solid fa-bars"></i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">Services</a></li>
+        <li><a href="collapsible.html">Pricing</a></li>
+        <li><a href="mobile.html">Blog</a></li>
+        <a href="#" class = "btn blue white-text">LOGIN</a>
+        <a href="#" style = "border:1px solid white" class = "black-text white btn">GET STARTED</a>
+      </ul>
+    </div>
+  </nav>`,
+  mobiledemo : ` <li><a href="sass.html">Sass</a></li>
+  <li><a href="badges.html">Components</a></li>
+  <li><a href="collapsible.html">Javascript</a></li>
+  <li><a href="mobile.html">Mobile</a></li>`,
+}
+export default Header;
