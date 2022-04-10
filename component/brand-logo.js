1ff19e9b2120d393c89e4ba3f380895dba029b4f
Hello Isf <23234234@gmail.com>
2022-04-11 02:33:20 +0530

    new branch created

--- component/brand-logo.js
+++
@@ -1,9 +0,0 @@
-const Brandlogo = {
-    brandLogo : `   <div id="brand-img">
-    <img src="./content/image/brand_img_1.svg" alt="">
-<img src="./content/image/brand_img_1.svg" alt="">
-<img src="./content/image/brand.svg" alt="">
-<img src="./content/image/brand_img_4.svg" alt="">
-</div>`
-};
-export default Brandlogo;
